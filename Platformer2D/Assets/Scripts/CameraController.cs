5402cd2ea1afa65feaee0f2f1d084651a0da00c1
Michael Ward <35927145+S00182367@users.noreply.github.com>
2021-06-13 19:28:36 +0100

    parallax
    parallx  added

--- Platformer2D/Assets/Scripts/CameraController.cs
+++ Platformer2D/Assets/Scripts/CameraController.cs
@@ -6,11 +6,15 @@ public class CameraController : MonoBehaviour
 {
     //Variables
     public Transform target;
+    public Transform farBackground;
+    public Transform middleBackground;
+    private float lastXPosition;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        //give first value
+        lastXPosition = transform.position.x;
     }
 
     // Update is called once per frame
@@ -18,5 +22,14 @@ public class CameraController : MonoBehaviour
     {
         //move the position of the camera
         transform.position = new Vector3(target.position.x, transform.position.y, transform.position.z);
+
+        //How much the camera is moving each frame
+        float distanceToMovex = transform.position.x - lastXPosition;
+
+        farBackground.position = farBackground.position + new Vector3(distanceToMovex, 0f, 0f);
+        middleBackground.position = middleBackground.position + new Vector3(distanceToMovex * .5f, 0f, 0f);
+      //middleBackground.position += new Vector3()  ... short cut
+
+        lastXPosition = transform.position.x;
     }
 }
