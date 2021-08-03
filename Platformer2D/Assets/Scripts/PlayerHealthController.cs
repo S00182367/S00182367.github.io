2ac4499f359202ab816df1118d9600525f9e2c45
Michael Ward <35927145+S00182367@users.noreply.github.com>
2021-08-03 20:36:47 +0100

    Invincible
    No damage for a period when you get hit

--- Platformer2D/Assets/Scripts/PlayerHealthController.cs
+++ Platformer2D/Assets/Scripts/PlayerHealthController.cs
@@ -9,10 +9,17 @@ public class PlayerHealthController : MonoBehaviour
     public int currentHealth;
     public int maxHealth;
 
+    public float invincibleLength; //adding invincblity to the player
+    private float invincibleCounter;
+
+    private SpriteRenderer theSR;
+
     //activates before void start connected to public static PlayerHealthController instance;
     private void Awake()
     {
         instance = this;
+
+        theSR = GetComponent<SpriteRenderer>(); // get the spriterenderer component
     }
 
     // Start is called before the first frame update
@@ -26,26 +33,47 @@ public class PlayerHealthController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(invincibleCounter > 0)
+        {
+            invincibleCounter -= Time.deltaTime; // time to get from 1 frame to next
+
+            if(invincibleCounter <= 0)
+            {
+                //when invinciblety runs out players color will retun to normal
+                theSR.color = new Color(theSR.color.r, theSR.color.g, theSR.color.b, 1f);
+            }
+        }
     }
 
     //Create own custom function
     public void DealDamage()
     {
-        //currentHealth = currentHealth - 1; long way
-        //currentHealth -= 1;
-        currentHealth--; //shorter way to do it -- means take away 1, ++ means add 1
-
-        //check what will happen when the player has no health
-        if(currentHealth <= 0)
+        if (invincibleCounter <= 0) // rest will only happen if invincble counter <= 0
         {
-            //if current health should go below zero
-            currentHealth = 0; // lowest health can be is zero
 
-            //remove the player game object from the game
-            gameObject.SetActive(false);
+            //currentHealth = currentHealth - 1; long way
+            //currentHealth -= 1;
+            currentHealth--; //shorter way to do it -- means take away 1, ++ means add 1
+
+            //check what will happen when the player has no health
+            if (currentHealth <= 0)
+            {
+                //if current health should go below zero
+                currentHealth = 0; // lowest health can be is zero
+
+                //remove the player game object from the game
+                gameObject.SetActive(false);
+            }
+            else
+            {
+                //setting the invincible counter
+                invincibleCounter = invincibleLength;
+
+                //when player takes damage change the color of character to show there in ivincible at that moment
+                theSR.color = new Color(theSR.color.r, theSR.color.g, theSR.color.b, .5f); //changes the alpha colour
+            }
+
+            UIController.instance.UpdateHealthDisplay();
         }
-
-        UIController.instance.UpdateHealthDisplay();
     }
 }
