47196ff946a83a3f8ad80f721acd97ec5516fe54
Michael Ward <35927145+S00182367@users.noreply.github.com>
2021-08-03 22:35:34 +0100

    checkpoints

---
+++ Platformer2D/Assets/Scripts/Checkpoint.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+    public SpriteRenderer theSR;
+
+    public Sprite cpOn;
+    public Sprite cpOff;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Player")) //better for more objects instead of  if(other.tag == "Player"
+        {
+            CheckpointController.instance.DeactivateCheckpoints();
+
+            theSR.sprite = cpOn; // activate the check point
+        }
+        
+    }
+
+    public void ResetCheckpoint()
+    {
+        theSR.sprite = cpOff; // turn off checkpoint
+    }
+}
---
+++ Platformer2D/Assets/Scripts/CheckpointController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckpointController : MonoBehaviour
+{
+    public static CheckpointController instance; // create instance of CheckpointController
+
+    private Checkpoint[] checkpoints; //use an array as there are to many objects to be keeping track of when it comes to checkpoints
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //find all the checkpoints within the scene when the game starts
+        checkpoints = FindObjectsOfType<Checkpoint>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void DeactivateCheckpoints()//activate current checkpoint and deactivate older checkpoint
+    {
+        for(int i = 0; i < checkpoints.Length; i++)// start at 0, check lenghth of array, add 1 to i until i < is exceeded
+        {
+            checkpoints[i].ResetCheckpoint(); //checkpoints at position i run function
+        }
+    }
+}
